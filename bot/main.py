16b36c8d4579922450806e74f931a74206a9d6d3
alteregog1 <88210725+alteregog1@users.noreply.github.com>
2021-08-15 12:02:59 +0700

    initial set

---
+++ bot/main.py
@@ -0,0 +1,51 @@
+import os, random, asyncio
+import discord
+from discord.ext import commands
+import requests
+from discord_slash import SlashCommand, SlashContext
+
+client = commands.Bot(command_prefix=".")
+slash = SlashCommand(client, sync_commands=True)
+
+coingecko = "https://api.coingecko.com/api/v3/simple/price?ids=plant-vs-undead-token&vs_currencies=usd,idr,bnb"
+WAIT_DURATION = int(os.environ['WAIT_DURATION'])
+TOKEN = os.environ['DISCORD_TOKEN']
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    print(f"$ {getPvuPerUSD()} - RP {getPvuPerIDR()} - BNB {getPvuPerBNB()}")
+    asyncio.create_task(taskUpdateActivity())
+
+
+def getCoingeckoData():
+    response = requests.get(coingecko)
+    data = response.json()
+    return data
+
+
+def getPvuPerUSD():
+    return float(getCoingeckoData()["plant-vs-undead-token"]['usd'])
+
+
+def getPvuPerIDR():
+    return float(getCoingeckoData()["plant-vs-undead-token"]['idr'])
+
+
+def getPvuPerBNB():
+    return float(getCoingeckoData()["plant-vs-undead-token"]['bnb'])
+
+
+async def taskUpdateActivity():
+    await client.wait_until_ready()
+    while not client.is_closed():
+        for guild in client.guilds:
+
+            await guild.me.edit(nick="${:.2f}".format(getPvuPerUSD())+"/PVU")
+
+        activityStatus = random.choice(["{:,}".format(round(getPvuPerIDR())) + " IDR", f"{getPvuPerBNB()} BNB"])
+
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activityStatus))
+        await asyncio.sleep(WAIT_DURATION)
+
+client.run(TOKEN)
